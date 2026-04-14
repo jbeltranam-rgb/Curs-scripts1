3f00d73e24fa18e62d83ad030ef57581431dde43
jbeltranam-rgb <jbeltranam@esliceu.net>
2026-04-14 18:55:47 +0200

    crear usuari

--- 005.sh
+++ 005.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
-if [[ ${UID} -ne 0 ]]
-then
+function usage(){
     echo "Usage ${0}"
     echo
     echo "has de ser root"
     exit 1
 
+}
+if [[ ${UID} -ne 0 ]]
+then
+    usage
 fi
 
 read -p "Introdueix el nom d'usuari: " USER_NAME
