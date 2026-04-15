eed130f76a138816d49d7309761fc768c07719da
jbeltranam-rgb <jbeltranam@esliceu.net>
2026-04-15 16:08:17 +0200

    crear usuari

--- 005.sh
+++ 005.sh
@@ -11,6 +11,12 @@ then
     usage
 fi
 
+read -p "Introdueix el nom complet: " COMMENTS
+#demanam el nom de l'usuari
 read -p "Introdueix el nom d'usuari: " USER_NAME
+#demanam el password
+read -p "Introdueix el password: " PASSWORD
 
 echo "creant l'usauri ${USER_NAME}"
+
+useradd -m -c "${COMMENTS}" ${USER_NAME}
