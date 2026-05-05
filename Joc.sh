43a1df5c4b05bad42661e4718430c4d2bb749820
jbeltranam-rgb <jbeltranam@esliceu.net>
2026-05-05 17:23:53 +0200

    Primer joc

--- Joc.sh
+++ Joc.sh
@@ -4,7 +4,7 @@ NUMERO=$((RANDOM % 100 + 1))
 INTENTS=5
 while [ ??? ]; do
 read -p "Entra un nombre (1-100): " RESPOSTA
-# Comparar RESPOSTA amb NUMERO i donar la pista (mes gran / mes pet
+
 if [ $NUMERO -eq $RESPOSTA ] 
 then
     echo "as guanyat"
@@ -14,6 +14,5 @@ elif [ $RESPOSTA -gt $NUMERO ]
     
 fi
 
-# Actualitzar el comptador d'intents
+
 done
-# Missatge final: ha encertat o ha esgotat els intents?
